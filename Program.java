2f114b833cfbdb16dd325c30577d73078823a2a7
Chaitanya Wangikar <99636144+chaitanyawangikar@users.noreply.github.com>
2024-03-16 20:50:22 +0530

    Add files via upload

--- Program.java
+++ Program.java
@@ -3,17 +3,9 @@ package Demo2;
 import java.util.Scanner;
 
 public class Program {
-   public static void main(String[] args) {
-	int a = 42;
-	double b = a;
-	float c = a;
-	boolean d = (a != 0);
-	String str = String.valueOf(a);
-	
-	
-	System.out.println(b); // Widening
-	System.out.println(c); // Widening
-	System.out.println(d); // Widening
-	System.out.println(str); // Widening
-}
+	public static void main(String[] args) {
+		int a = 42;
+		double b = a;
+		System.out.println(b); // Widening
+	}
 }
